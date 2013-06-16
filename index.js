74020318145c7e0a8b8f5b96cb8c223929a6d8e3
Drew Harris <drewbharris@gmail.com>
2013-06-16 09:26:39 -0700

    Readme

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server, {
+        'log': false
+    });
+
+var values = [0, 0, 0, 0],
+    sockets = {};
+
+app.use("/js", express.static(__dirname + '/web/js'));
+app.use("/css", express.static(__dirname + '/web/css'));
+app.use(express.bodyParser());
+
+app.get('/', function(req, res){
+    fs.readFile(__dirname + "/web/index.html", "UTF-8", function(err, data){
+        res.send(data);
+    });
+});
+
+var writeToClients = function(){
+	Object.keys(sockets).map(function(socket){
+		sockets[socket].emit('data', {
+			'values': values
+		});
+	});
+};
+
+io.sockets.on('connection', function (socket) {
+    sockets[socket.id] = socket;
+
+    socket.on('update', function(data){
+    	values = data.values;
+        writeToClients(data.element, data.value);
+    });
+    socket.on('updateAll', function(data){
+    	values = data.values;
+        writeToClients(data.values);
+    });
+
+    socket.on('disconnect', function(){
+        delete sockets[socket.id];
+    });
+});
+
+server.listen(8001);
+console.log('listening on 8001');
